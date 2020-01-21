d5014d209950941f6acc14a109fc2d1f5dcc00d7
hassanjamiltpl <hassan.jamil@tplmaps.com>
2020-01-21 09:53:07 +0500

    - Added Week 9 Samples - API 28 Picasso not working fix - Ripple Effect or recyclerView items - Change in Styles>AppTheme

--- app/src/main/java/com/evs/android/mysampleapp/MainActivity.java
+++ app/src/main/java/com/evs/android/mysampleapp/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.evs.android.mysampleapp.week6.Week6Activity;
 import com.evs.android.mysampleapp.week7.Week7Activity;
 import com.evs.android.mysampleapp.week8.Week8Activity;
+import com.evs.android.mysampleapp.week9.Week9Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Week8Activity.class));
+            }
+        });
+
+        findViewById(R.id.btnWeek9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Week9Activity.class));
             }
         });
     }
---
+++ app/src/main/java/com/evs/android/mysampleapp/week9/animate/bitmaps/AnimateBitmapsActivity.java
@@ -0,0 +1,28 @@
+package com.evs.android.mysampleapp.week9.animate.bitmaps;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.evs.android.mysampleapp.R;
+
+public class AnimateBitmapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animate_bitmaps);
+
+        ImageView ivBitmapAnimation = findViewById(R.id.ivBitmapAnimation);
+        ivBitmapAnimation.setBackgroundResource(R.drawable.animation_back);
+        ivBitmapAnimation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((AnimationDrawable) ivBitmapAnimation.getBackground()).start();
+            }
+        });
+    }
+}
